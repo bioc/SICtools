c98369a2c53497e65f278d2e885566990d096965
Xiaobin Xing <xiaobinxing0316@gmail.com>
2016-03-31 07:31:26 +0000

    debug for uniTest
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SICtools@115482 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_snpDiff.R
+++ inst/unitTests/test_snpDiff.R
@@ -8,12 +8,12 @@ test_snpDiff <- function(){
 	refFsa <- system.file(package='SICtools','extdata','example.ref.fasta')
 	
 	## check
-	snpTest <- snpDiff(bam1,bam2,refFsa,'chr04',962501,1026983,pValueCutOff=1,baseDistCutOff=0)
+	snpTest <- snpDiff(bam1,bam2,refFsa,'chr04',962501,1026983,pValueCutOff=1,baseDistCutOff=0,nCores=2)
 	checkEquals(nrow(snpTest),16)
 	checkEquals(subset(snpTest,p.value < 1e-10)$pos,c(962801,1026683))
 	
 	## check NULL
-	checkTrue(is.null(snpDiff(bam1,bam2,refFsa,'chr04',962501,1026983,pValueCutOff=0,baseDistCutOff=0)))
+	checkTrue(is.null(snpDiff(bam1,bam2,refFsa,'chr04',962501,1026983,pValueCutOff=0,baseDistCutOff=0,nCores=2)))
 	
 }
 
